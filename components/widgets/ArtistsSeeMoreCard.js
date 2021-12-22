f7648fd01e28c7feb5925944f93af20e24bbb1ac
Aymen Fezai <fezaiaymen1994@gmail.com>
2021-12-22 17:08:48 +0100

    FIX

--- components/widgets/ArtistsSeeMoreCard.js
+++ components/widgets/ArtistsSeeMoreCard.js
@@ -1,70 +1,68 @@
 const ArtistsSeeMoreCard = () => {
-    return (
-        <>
-            <a href="/artists">
-                <div className="workshops-card">
-                    <h1>Artists</h1>
-                    <p>consectetur adipiscing elit. Cras ac ante quis est consequat tristique sed ac purus.</p>
+	return (
+		<>
+			<a href='/artists'>
+				<div className='workshops-card'>
+					<h1>Artists</h1>
+					{/* <p>consectetur adipiscing elit. Cras ac ante quis est consequat tristique sed ac purus.</p> */}
 
-                    <div className="btn">
-                        See more
-                    </div>
-                </div>
-            </a>
+					<div className='btn'>See more</div>
+				</div>
+			</a>
 
-            <style jsx>{`
-                .workshops-card {
-                    width: 100%;
-                    height: 100%;
-                    background: url("/img/bg-artist-card.png");
-                    background-repeat: no-repeat;
-                    background-position: center;
-                    background-size: cover;
-                    padding: 20px 33px;
-                    border-radius: 25px;
-                    cursor: pointer;
-                    transition: all .5s ease;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                }
+			<style jsx>{`
+				.workshops-card {
+					width: 100%;
+					height: 100%;
+					background: url("/img/bg-artist-card.png");
+					background-repeat: no-repeat;
+					background-position: center;
+					background-size: cover;
+					padding: 20px 33px;
+					border-radius: 25px;
+					cursor: pointer;
+					transition: all 0.5s ease;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+				}
 
-                .workshops-card:hover {
-                    box-shadow: inset  0px 320px 0px 0 rgb(0, 0, 0, .2);
-                }
+				.workshops-card:hover {
+					box-shadow: inset 0px 320px 0px 0 rgb(0, 0, 0, 0.2);
+				}
 
-                h1 {
-                    font-family: "Poppins";
-                    font-weight: 600;
-                    font-size: 62px;
-                    line-height: 72px;
-                    color: #fff;
-                }
+				h1 {
+					font-family: "Poppins";
+					font-weight: 600;
+					font-size: 62px;
+					line-height: 72px;
+					color: #fff;
+				}
 
-                p {
-                    font-family: "Poppins";
-                    font-weight: 300;
-                    font-size: 22px;
-                    line-height: 25px;
-                    color: #fff;
-                }
+				p {
+					font-family: "Poppins";
+					font-weight: 300;
+					font-size: 22px;
+					line-height: 25px;
+					color: #fff;
+				}
 
-                .btn {
-                    width: fit-content;
-                    border-radius: 25px;
-                    background: #fff;
-                    padding: 12px 40px;
-                    font-family: "Poppins";
-                    font-weight: 500;
-                    font-size: 18px;
-                    line-height: 28px;
-                    text-align: center;
-                    color: #ed1e46;
-                    margin-top: 40px;
-                }
-            `}</style>
-        </>
-    )
-}
+				.btn {
+					width: fit-content;
+					border-radius: 25px;
+					background: #fff;
+					padding: 12px 40px;
+					font-family: "Poppins";
+					font-weight: 500;
+					font-size: 18px;
+					line-height: 28px;
+					text-align: center;
+					color: #ed1e46;
+					margin-top: 40px;
+				}
+			`}</style>
+		</>
+	);
+};
 
 export default ArtistsSeeMoreCard;
